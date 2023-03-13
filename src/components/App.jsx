b0a19674e679f696641284438e1b52e5b5395429
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2023-03-13 12:40:04 +0200

    add react-router-dom and test linters

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import { KeyBoard } from './KeyBoard.jsx'
+import { Footer } from './Footer.jsx'
+import { Header } from './Header.jsx'
 
 export const App = () => {
 	const startText =
 		'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam odio inventore, aut necessitatibus a sed sapiente repudiandae, sunt amet consequuntur iste illum iusto culpa id excepturi, cupiditate reprehenderit ratione laudantium.'
-	//   const startText = "Lorem, ipsum dolor";
 	return (
-		<>
-			<main className='page'>
-				<KeyBoard startText={startText} />
-			</main>
-		</>
+		<main className='page'>
+			<Header />
+			<Footer />
+		</main>
 	)
 }
---
+++ src/pages/Trainer.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { KeyBoard } from '../components/KeyBoard'
+
+export const Trainer = ({ startText }) => {
+	return <KeyBoard startText={startText} />
+}
